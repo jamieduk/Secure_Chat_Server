fc85ce013f9ea279bc3a845e4e4dfa8c5af1c9b7
Jay <jamied_uk@hotmail.com>
2024-02-19 05:52:59 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -6,14 +6,15 @@ sudo apt install -y sshpass
 dlpath="/home/$USER/Documents/Scripts/Bash/Secure_Chat_Server" # Downloads
 echo "Setting Up Secure Chat Room Server & Client"
 cd $dlpath
-wget -c https://github.com/shazow/ssh-chat/releases/download/v1.6/ssh-chat-linux_amd64.tgz
-tar -xvf ssh-chat-linux_amd64.tgz
-cd ssh-chat/
+#wget -c https://github.com/shazow/ssh-chat/releases/download/v1.6/ssh-chat-linux_amd64.tgz
+#tar -xvf ssh-chat-linux_amd64.tgz
+#cd ssh-chat/
 touch admins.txt
-./ssh-chat
-cd ..
+
+
 sudo chmod +x *.sh
 
 echo "Whats Your Email? (Used for keygen creating for server side...)"
 read email
 ssh-keygen -t rsa -b 4096 -C "$email" -f $HOME/.ssh/id_dsa
+#./ssh-chat
