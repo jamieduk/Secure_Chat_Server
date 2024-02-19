3aa556443f8eb13e5e80f54b1eb8fad7d1647e94
Jay <jamied_uk@hotmail.com>
2024-02-19 06:07:28 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
-echo "Setting Up Secure Chat 2023 "
+echo "Setting Up Secure Chat (c)J~Net 2023 "
 sudo apt install -y sshpass
 
+sudo chmod +x *.sh
 
-dlpath="/home/$USER/Documents/Scripts/Bash/Secure_Chat_Server" # Downloads
+dlpath=$(pwd)
+
 echo "Setting Up Secure Chat Room Server & Client"
-cd $dlpath
+#cd $dlpath
 #wget -c https://github.com/shazow/ssh-chat/releases/download/v1.6/ssh-chat-linux_amd64.tgz
 #tar -xvf ssh-chat-linux_amd64.tgz
 #cd ssh-chat/
 touch admins.txt
 
+# Check CPU Type
+#!/bin/bash
 
-sudo chmod +x *.sh
+# Get the CPU architecture
+cpu_arch=$(uname -m)
+
+# Check if the CPU architecture is arm64
+if [ "$cpu_arch" == "aarch64" ]; then
+    cputype="arm64"
+    cp arm64/ssh-chat $dlpath
+# Check if the CPU architecture is amd64 (x86_64)
+elif [ "$cpu_arch" == "x86_64" ]; then
+    cputype="amd64"
+    cp amd64/ssh-chat $dlpath
+else
+    # If it's neither arm64 nor amd64, you can set it to another value or handle it accordingly
+    cputype="unknown"
+fi
+echo "CPU Detected: $cputype"
+#
 
 echo "Whats Your Email? (Used for keygen creating for server side...)"
 read email
